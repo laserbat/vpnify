d09899e511ad17d9b392c1059547c87221343e18
Olga Ustuzhanina <me@laserbat.pw>
2019-04-23 06:33:07 +0700

    Fixed a bug with mount namespaces not working propertly, some minor updates to example hooks

--- example-hooks/post.d/unvpn.sh
+++ example-hooks/post.d/unvpn.sh
@@ -1,8 +1,8 @@
 # Try to find a second routing table
-TABLE=$(egrep '^1\s+\w+' /etc/iproute2/rt_tables | sed -E 's/1\s+//g')
+TABLE="$(egrep '^1\s+\w+' /etc/iproute2/rt_tables | sed -E 's/1\s+//g')"
 
 # Drop the routing rule 
-ip rule del iif $VETH0 table rt2
+ip rule del iif "$VETH0" table rt2
 
 # Flush routes 
 ip route flush table rt2 
--- example-hooks/pre.d/unvpn.sh
+++ example-hooks/pre.d/unvpn.sh
@@ -1,5 +1,5 @@
 # Try to find a second routing table
-TABLE=$(egrep '^1\s+\w+' /etc/iproute2/rt_tables | sed -E 's/1\s+//g')
+TABLE="$(egrep '^1\s+\w+' /etc/iproute2/rt_tables | sed -E 's/1\s+//g')"
 
 # Create it if it doesn't exist
 if [[ -z "$TABLE" ]]; then
@@ -8,7 +8,7 @@ if [[ -z "$TABLE" ]]; then
 fi
 
 # Use it for traffic coming out of our namespace
-ip rule add iif $VETH0 table rt2
+ip rule add iif "$VETH0" table rt2
 
 # Replace this with appropriate default gateway
 ip route add default via 192.168.1.1 table rt2
